c2b29dfe5be7e3d3301015011347eec9c67e2dbc
williamarcondes <william.marcondes@betrybe.com>
2021-10-19 18:25:03 -0300

    Exemplos Iniciais

---
+++ python/1000/1002.py
@@ -0,0 +1,8 @@
+# a, b, c = [int(x) for x in input().split(" ")]
+
+a = int(input())
+b = int(input())
+
+x = a + b
+
+print(f'X = {x}')
---
+++ python/1000/1003.py
@@ -0,0 +1,6 @@
+pi = 3.14159
+raio = float(input())
+
+area = round(pi * pow(raio, 2), 4)
+
+print("A={:.4f}".format(area))
